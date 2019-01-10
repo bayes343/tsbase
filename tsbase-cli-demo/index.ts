e07946697556c7cc6981d5d7fc0cb2fbc3f77d05
Joseph Bayes <joseph.bayes@modea.com>
2019-01-10 17:04:57 -0500

    Reversed changes to cli app.

--- tsbase-cli-demo/index.ts
+++ tsbase-cli-demo/index.ts
@@ -2,7 +2,6 @@
 
 import { List, NodeXhrRequestHandler, JsonSerializer } from 'tsbase';
 import { HttpClient } from 'tsbase';
-import { LoanResults } from './domain/LoanResults';
 
 const readline = require('readline').createInterface({
   input: process.stdin,
@@ -20,28 +19,12 @@ class Program {
     const questions = [
       {
         type: 'input',
-        name: 'principal',
-        message: "Enter principal"
-      },
-      {
-        type: 'input',
-        name: 'term',
-        message: "Enter term"
-      },
-      {
-        type: 'input',
-        name: 'apr',
-        message: "Enter apr"
-      },
-      {
-        type: 'input',
-        name: 'frequency',
-        message: "Enter frequency in months"
+        name: 'uri',
+        message: "Type a uri"
       }
     ];
     inquirer.prompt(questions).then(answers => {
-      // this.printResponseFrom(`${answers['base']}/${answers['uri']}`);
-      this.printResponseFrom(`Loan/Results?principal=${answers['principal']}&term=${answers['term']}&apr=${answers['apr']}&frequency=${answers['frequency']}`);
+      this.printResponseFrom(answers['uri']);
     });
   }
 
@@ -50,15 +33,9 @@ class Program {
     const xhrRequestHandler = new NodeXhrRequestHandler();
     const client = new HttpClient(xhrRequestHandler);
     xhrRequestHandler.HttpClient = client;
-    client.BaseAddress = 'https://prod-wbcw-api.azurewebsites.net/api';
+    client.BaseAddress = 'https://foaas.com';
     const response = await client.GetStringAsync(uri);
-
-    // Serialize and use instance
-    const serializer = new JsonSerializer<LoanResults>();
-    const loanResults: LoanResults = serializer.Serialize(LoanResults, JSON.parse(response));
-    for (const element of loanResults.amortizationYearlySchedule) {
-      console.log(element);
-    }
+    console.log(response);
   }
 }
 
