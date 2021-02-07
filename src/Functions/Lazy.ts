16cbcf4e48d9a3b4bf87607a2499eeaf883a380a
Joey Bayes <joey.bayes@uscca.com>
2021-02-06 23:12:56 -0500

    Add docs for Lazy function.

--- src/Functions/Lazy.ts
+++ src/Functions/Lazy.ts
@@ -1,3 +1,8 @@
+/**
+ * Returns a function which will create an instance of the given type on the first
+ * invocation, and then continue to return that instance on subsequent invocations
+ * @param t Type being lazy loaded
+ */
 export function Lazy<T>(t: { new(): T; }): () => T {
   let instance: T | null = null;
 
