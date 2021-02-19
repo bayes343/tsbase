0a03d79b2b6c085332085185e2f6cb8a86223c5e
Joey Bayes <joey.bayes@uscca.com>
2021-02-18 22:41:56 -0500

    Add docs to logger.

--- src/Utility/Logger/Logger.ts
+++ src/Utility/Logger/Logger.ts
@@ -2,9 +2,20 @@ import { Observable } from '../../Patterns/Observable/Observable';
 import { LogEntry } from './LogEntry';
 
 export class Logger {
+  /**
+   * All logs collected during the current application lifecycle
+   */
   public static LogEntries = new Array<LogEntry>();
+
+  /**
+   * Observable that is published to upon logging a new entry
+   */
   public static EntryLogged = new Observable<LogEntry>();
 
+  /**
+   * Log a new entry
+   * @param entry
+   */
   public static Log(entry: LogEntry): void {
     Logger.LogEntries.push(entry);
     Logger.EntryLogged.Publish(entry);
