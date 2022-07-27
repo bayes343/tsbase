8505ea1f0a574aa06c3529629b48ec10c98badcf
Joey Bayes <joey.bayes@uscca.com>
2022-07-27 01:24:41 -0400

    Implement other methods on cookie storage.

--- src/Persistence/GenericStorage/CookieStorage.ts
+++ src/Persistence/GenericStorage/CookieStorage.ts
@@ -1,5 +1,6 @@
-import { Command, GenericResult, Result } from '../../Patterns/module';
-import { Strings } from '../../public_api';
+import { Strings } from '../../System/Strings';
+import { GenericResult, Result } from '../../Patterns/Result/module';
+import { Command, Query } from '../../Patterns/CommandQuery/module';
 import { JsonSerializer } from '../../Utility/Serialization/JsonSerializer';
 import { IGenericStorage } from './IGenericStorage';
 
@@ -9,32 +10,52 @@ import { IGenericStorage } from './IGenericStorage';
 export class CookieStorage implements IGenericStorage {
   constructor(
     private serializer = new JsonSerializer(),
-    private getCookie = () => document.cookie
+    private mainDocument = document
   ) { }
 
   public Get<T>(type: new () => T, key: string): GenericResult<T> {
-    throw new Error('Method not implemented.');
+    return new Query(() => {
+      const value = this.GetValue(key).Value;
+      if (value) {
+        return this.serializer.Serialize(type, JSON.parse(value)) as T;
+      } else {
+        throw new Error(`Unable to retrieve "${key}"`);
+      }
+    }).Execute();
   }
 
-  public Set<T>(key: string, value: T): Result {
-    throw new Error('Method not implemented.');
+  public Set<T>(key: string, value: T, expirationDate?: Date): Result {
+    return new Command(() => {
+      this.SetValue(key, JSON.stringify(value), expirationDate);
+    }).Execute();
   }
 
-  public GetValue(key: string): GenericResult<any> {
-    throw new Error('Method not implemented.');
+  public GetValue(key: string): GenericResult<string> {
+    return new Query(() => {
+      const cookiePairs = this.mainDocument.cookie.split(';');
+      const cookieKeys = cookiePairs.map(c => c.split('=')[0]);
+      const cookieValues = cookiePairs.map(c => c.split('=')[1]);
+      const keyIndex = cookieKeys.indexOf(key);
+
+      if (keyIndex >= 0) {
+        return cookieValues[keyIndex];
+      } else {
+        throw new Error(`Unable to retrieve "${key}"`);
+      }
+    }).Execute();
   }
 
   public SetValue(key: string, value: string, expirationDate?: Date): Result {
     return new Command(() => {
       const newCookie = `${key}=${value};${expirationDate ?
         `expires=${expirationDate.toUTCString()}` : Strings.Empty};path=/`;
-      document.cookie = newCookie;
+      this.mainDocument.cookie = newCookie;
     }).Execute();
   }
 
   public Remove(key: string): Result {
     return new Command(() => {
-      document.cookie = `${key}=;expires=${new Date(0).toUTCString()};path=/`;
+      this.mainDocument.cookie = `${key}=;expires=${new Date(0).toUTCString()};path=/`;
     }).Execute();
   }
 }
