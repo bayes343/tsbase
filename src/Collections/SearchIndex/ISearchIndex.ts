8b9feeff359658b8a97f15d66b12b8b5950bb209
Joey Bayes <joey.bayes@uscca.com>
2023-09-23 00:15:11 -0400

    Document ISearchIndex interface.

--- src/Collections/SearchIndex/ISearchIndex.ts
+++ src/Collections/SearchIndex/ISearchIndex.ts
@@ -1,13 +1,53 @@
+/**
+ * Function which determines whether the result will be considered for a given query
+ */
 export type Qualifier = (query: string) => boolean;
+
+/**
+ * Data structure containing the subject (T) of the search as well as a "qualifier" function
+ */
 export type SearchResult<T> = {
   item: T,
   qualifier: Qualifier
 };
+
+/**
+ * Function which generates indexes or queries from data (D) and their corresponding results (T)
+ */
 export type Indexer<D, T> = (d: D) => [string, SearchResult<T>][];
 
+/**
+ * A collection where all representations of data are indexed upon insertion for efficient querying based on given configuration
+ * Use cases include:
+ * - Search feature which returns relevant results with the option to support "autocomplete" functionality
+ * - Answering potentially complex but predictable questions based on parameters of an "indexer" function
+ */
 export interface ISearchIndex<T> {
+  /**
+   * Insert elements into the index <string, T> using the given indexer function
+   * @param data
+   * @param indexer
+   */
   Insert<D>(data: D[], indexer: Indexer<D, T>): Promise<void>;
-  AutoComplete(query: string, limit?: number): Promise<string[]>;
+  /**
+   * Return the best match indexes for a given query (consider for an autocomplete feature)
+   * @param query
+   * @param limit
+   */
+  GetIndexesForQuery(query: string, limit?: number): Promise<string[]>;
+  /**
+   * Return the best results for a given query
+   * @param query
+   * @param limit
+   */
   Search(query: string, limit?: number): Promise<T[]>;
+  /**
+   * Return the best match (equivalent to using the top result from a search)
+   * @param query
+   */
+  Answer(query: string): Promise<T | null>;
+  /**
+   * Deletes all indexed data
+   */
   Reset(): void;
 }
--- src/Collections/SearchIndex/SearchIndex.ts
+++ src/Collections/SearchIndex/SearchIndex.ts
@@ -4,7 +4,7 @@ import { ISearchIndex, SearchResult, Indexer } from './ISearchIndex';
 export class SearchIndex<T> implements ISearchIndex<T> {
   private index: Record<string, SearchResult<T>[]> = {};
 
-  async Insert<D>(items: D[], indexer: Indexer<D, T>): Promise<void> {
+  public async Insert<D>(items: D[], indexer: Indexer<D, T>): Promise<void> {
     items.forEach(item => {
       const entries = indexer(item);
       entries.forEach(entry => {
@@ -14,11 +14,11 @@ export class SearchIndex<T> implements ISearchIndex<T> {
     });
   }
 
-  async AutoComplete(query: string, limit = 10): Promise<string[]> {
+  public async GetIndexesForQuery(query: string, limit = 10): Promise<string[]> {
     return Queryable.From(Object.keys(this.index)).Search(query).slice(0, limit);
   }
 
-  async Search(query: string, limit = 10): Promise<T[]> {
+  public async Search(query: string, limit = 10): Promise<T[]> {
     const matchingIndexes = Queryable.From(Object.keys(this.index)).Search(query);
     let results: T[] = [];
     matchingIndexes.forEach(i => {
@@ -28,7 +28,12 @@ export class SearchIndex<T> implements ISearchIndex<T> {
     return Queryable.From(results).Distinct().slice(0, limit);
   }
 
-  Reset(): void {
+  public async Answer(query: string): Promise<T | null> {
+    const results = await this.Search(query, 1);
+    return results[0] || null;
+  }
+
+  public Reset(): void {
     this.index = {};
   }
 }
