aae594ebb4e9430155d5105c73e337484cc681af
Joey Bayes <joey.bayes@uscca.com>
2023-09-25 23:28:05 -0400

    Write a couple more tests.

--- src/Collections/SearchIndex/tests/Scenario.spec.ts
+++ src/Collections/SearchIndex/tests/Scenario.spec.ts
@@ -2,6 +2,8 @@ import { Indexer } from '../ISearchIndex';
 import { SearchIndex } from '../SearchIndex';
 
 class Bot {
+  static Unknown = 'Sorry, I\'m not sure about that one...';
+
   constructor(
     private index = new SearchIndex<() => string>()
   ) { }
@@ -12,7 +14,7 @@ class Bot {
 
   Answer = async (question: string): Promise<string> => {
     const answerFunc = await this.index.Answer(question);
-    return answerFunc?.() || 'Sorry, I\'m not sure about that one...';
+    return answerFunc?.() || Bot.Unknown;
   };
 }
 
@@ -20,29 +22,57 @@ describe('Bot', () => {
   const bot = new Bot();
 
   beforeAll(() => {
-    bot.Program([
-      'The year is',
-      'WW2 Broke out in'
-    ], (d) => [
+    bot.Program([{}], () => [
       ['What year is it?', {
-        item: () => `${d}: ${new Date().getFullYear().toString()}`,
+        item: () => `The year is ${new Date().getFullYear().toString()}`,
         qualifier: (q) => ['what', 'year', 'is', 'it'].every(s => q.toLowerCase().toString().includes(s))
       }],
       ['What year did WW2 start?', {
-        item: () => `${d}: 1939`,
+        item: () => 'WW2 broke out in 1939',
         qualifier: (q) => ['what', 'year', 'ww2', 'start'].every(s => q.toLowerCase().includes(s))
       }]
-    ]
-    );
+    ]);
+
+    bot.Program([
+      {
+        firstName: 'John',
+        surname: 'Doe',
+        birthDate: new Date(1990, 8, 5)
+      },
+      {
+        firstName: 'Jane',
+        surname: 'Doe',
+        birthDate: new Date(1992, 3, 15)
+      }
+    ], (d) => [
+      [`How old is ${d.firstName} ${d.surname}?`, {
+        item: () => (Math.round((new Date(2023, 8, 25).getTime() - d.birthDate.getTime()) / 1000 / 60 / 60 / 24 / 365)).toString(),
+        qualifier: (q) => ['how', 'old'].every(s => q.toLowerCase().includes(s)) &&
+          [d.firstName.toLowerCase(), d.surname.toLowerCase()].some(s => q.toLowerCase().includes(s))
+      }]
+    ]);
   });
 
   it('should know what year it is', async () => {
     const answer = await bot.Answer('What year is it?');
-    expect(answer).toEqual(`The year is: ${new Date().getFullYear().toString()}`);
+    expect(answer).toEqual(`The year is ${new Date().getFullYear().toString()}`);
   });
 
   it('should know what year WW2 started', async () => {
     const answer = await bot.Answer('What year did WW2 start in?');
-    expect(answer).toContain('1939');
+    expect(answer).toEqual('WW2 broke out in 1939');
+  });
+
+  it('should not know how WW2 started?', async () => {
+    const answer = await bot.Answer('How did WW2 start?');
+    expect(answer).toEqual(Bot.Unknown);
+  });
+
+  it('should know how old John and Jane Doe are', async () => {
+    let answer = await bot.Answer('How old is John?');
+    expect(answer).toEqual('33');
+
+    answer = await bot.Answer('How old is Jane?');
+    expect(answer).toEqual('31');
   });
 });
