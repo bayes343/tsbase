a71620e6ce88e82d5d91f926c948a7a9cef20a25
Joey Bayes <joey.bayes@uscca.com>
2023-11-13 09:33:28 -0500

    Add cookie value "string" overload to Cookies.GetCookieMap method.

--- src/Utility/Web/Cookies.ts
+++ src/Utility/Web/Cookies.ts
@@ -1,12 +1,15 @@
 export class Cookies {
   private constructor() { }
 
+  public static GetCookieMap(mainDocument: Document): Map<string, string>;
+  public static GetCookieMap(cookieValue: string): Map<string, string>;
   public static GetCookieMap(
-    mainDocument = document
+    mainDocumentOrCookieValue: Document | string = document
   ): Map<string, string> {
+    const cookieValue = typeof mainDocumentOrCookieValue === 'string' ? mainDocumentOrCookieValue : mainDocumentOrCookieValue.cookie;
     const map = new Map<string, string>();
 
-    const cookiePairs = mainDocument.cookie.split(';').map(e => e.trim());
+    const cookiePairs = cookieValue.split(';').map(e => e.trim());
     const cookieKeys = cookiePairs.map(c => c.split('=')[0]);
     const cookieValues = cookiePairs.map(c => c.split('=')[1]);
     cookieKeys.forEach((key, i) => {
--- src/Utility/Web/tests/Cookies.spec.ts
+++ src/Utility/Web/tests/Cookies.spec.ts
@@ -1,8 +1,17 @@
 import { Cookies } from '../Cookies';
 
 describe('Cookies', () => {
-  it('should return a map of cookie key/value pairs', () => {
+  it('should return a map of cookie key/value pairs from document', () => {
     const map = Cookies.GetCookieMap({ cookie: 'one=1; two=2; three=3;4=four; ' } as Document);
+
+    expect(map.get('one')).toEqual('1');
+    expect(map.get('two')).toEqual('2');
+    expect(map.get('three')).toEqual('3');
+    expect(map.get('4')).toEqual('four');
+  });
+
+  it('should return a map of cookie key/value pairs from string', () => {
+    const map = Cookies.GetCookieMap('one=1; two=2; three=3;4=four; ');
 
     expect(map.get('one')).toEqual('1');
     expect(map.get('two')).toEqual('2');
